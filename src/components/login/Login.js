d161818aaedd4083942e70d3787bc5f43bfa28e0
Aaron Manwill <manwillaaron@gmail.com>
2019-07-20 14:06:12 -0700

    second saturday update

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './Login.css';
+import React, { Component } from "react";
+import "./Login.css";
 
-function Login() {
-  return (
-    <div>
-     Login
-    </div>
-  );
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleChange = (e) => {
+    const {value, name} = e.target
+    this.setState({[name]: value})
+    // console.log(value);
+  }
+
+  render() {
+    const {username, password} = this.state
+    return (
+      <div>
+        <div>
+          Username:{' '}
+          <input value = {username} 
+          onChange= {this.handleChange} 
+          name = 'username' 
+          type = 'text'/>
+        </div>
+        <div>
+          Password:{' '}
+          <input 
+          value = {password} 
+          onChange= {this.handleChange} 
+          name= 'password'
+          type = 'password'/>
+        </div>
+        <button>Login</button>
+      </div>
+    );
+  }
 }
 
 export default Login;
